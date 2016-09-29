a2e274a8f00534803495ef379294ad5dfaea7a94
ZhAhUa <939429643@qq.com>
2016-09-29 14:33:48 +0800

    TestCommit

--- gen/com/finance/familyfinance/R.java
+++ gen/com/finance/familyfinance/R.java
@@ -30,12 +30,13 @@ public final class R {
         public static final int ic_search=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int main_Flag=0x7f080005;
-        public static final int main_Home=0x7f080003;
-        public static final int main_Message=0x7f080004;
-        public static final int main_Self=0x7f080006;
-        public static final int main_tab_group=0x7f080002;
+        public static final int Btn_1=0x7f080002;
+        public static final int action_settings=0x7f080008;
+        public static final int main_Flag=0x7f080006;
+        public static final int main_Home=0x7f080004;
+        public static final int main_Message=0x7f080005;
+        public static final int main_Self=0x7f080007;
+        public static final int main_tab_group=0x7f080003;
         public static final int txt1=0x7f080000;
         public static final int txt2=0x7f080001;
     }
